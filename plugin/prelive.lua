ae87b2dc33d4fe7b648ed9f51828e33f316cb77c
hat0uma <55551571+hat0uma@users.noreply.github.com>
2024-07-29 17:57:13 +0900

    fix: PreliveGo command options.

--- plugin/prelive.lua
+++ plugin/prelive.lua
@@ -1,11 +1,44 @@
-vim.api.nvim_create_user_command("PreliveGo", function()
-  local dir = vim.uv.cwd() or ""
-  local file = vim.api.nvim_buf_get_name(0)
-  if file ~= "" then
-    file = vim.fn.fnamemodify(file, ":~:.")
+vim.api.nvim_create_user_command("PreliveGo", function(opts)
+  local params = vim.split(opts.args, "%s+", { trimempty = true })
+  if #params > 2 then
+    print("Too many arguments")
+    return
+  end
+
+  local dir, file ---@type string?,string?
+  dir, file = params[1], params[2]
+  dir = dir or vim.uv.cwd()
+  file = file or vim.fn.expand("%:~:.")
+
+  -- check directory
+  local err
+  dir, err = vim.uv.fs_realpath(vim.fs.normalize(dir))
+  if not dir then
+    vim.notify(err or (dir .. " is not found."))
+    return
+  end
+  if vim.fn.isdirectory(dir) == 0 then
+    vim.notify("Not a Directory " .. dir)
+    return
+  end
+
+  -- check file
+  if file == "" or not file then
+    file = nil
+  else
+    file, err = vim.uv.fs_realpath(vim.fs.normalize(file))
+    if not file then
+      -- can't access to the file
+      vim.notify(err or (file .. " is not found."))
+    elseif file:sub(1, #dir) == dir then
+      -- `file` is in the `dir`
+      file = file:sub(#dir + 1)
+    else
+      file = nil
+    end
   end
   require("prelive.api").go(dir, file)
-end, {})
+end, { nargs = "*", complete = "file" })
 
 vim.api.nvim_create_user_command("PreliveStatus", function()
   require("prelive.api").status()
